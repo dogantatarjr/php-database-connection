09f8b6a55e1f048398c26623c4910de85e7c4c92
Doğan <120167505+dogantatarjr@users.noreply.github.com>
2023-10-19 14:36:42 +0300

    connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 
-    $host = "localhost"; // Host Name (I prefer localhost for unfinished projects)
+    $host = "localhost"; // Host Name (I prefer localhost [XAMPP] for unfinished projects)
     $dbusername = "root"; // Host's Username (It is default "root" in localhost. It can be changed host to host)
     $dbpassword = ""; // Host's Password (Again can be changed host by host but default blank in localhost)
     $dbname = "dyk"; // Your Database's Name
